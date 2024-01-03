abab35abbc3c1a6a4303bf9d1f69656c55f3d2dd
Julius Polar <julius.polar@protonmail.com>
2024-01-03 15:19:22 +0100

    updated package.json

--- lib/input/input.stories.tsx
+++ lib/input/input.stories.tsx
@@ -17,6 +17,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const InputText: Story = {
+  name: "Input type text",
   args: {
     id: "input-id",
     label: "Input label",
@@ -27,6 +28,7 @@ export const InputText: Story = {
 };
 
 export const InputNumber: Story = {
+  name: "Input type number",
   args: {
     id: "input-id",
     label: "Input number",
@@ -37,6 +39,7 @@ export const InputNumber: Story = {
 };
 
 export const InputSearch: Story = {
+  name: "Input type search",
   args: {
     id: "input-id",
     label: "Input search",
@@ -47,6 +50,7 @@ export const InputSearch: Story = {
 };
 
 export const InputHiddenLabel: Story = {
+  name: "Input without label",
   args: {
     id: "input-id",
     label: "Input label",
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,17 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] })],
+  plugins: [
+    react(),
+    dts({
+      include: ["lib"],
+      exclude: [
+        "lib/**/*.stories.tsx",
+        "lib/**/*.test.tsx",
+        "lib/test-setup.ts",
+      ],
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
@@ -22,13 +32,13 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
       name: "scratchpad",
-      fileName: "main",
+      fileName: "scratchpad",
       formats: ["es"],
     },
     rollupOptions: {
       external: [
         "react",
-        "react/jsx-runtime",
+        "react-dom",
         "@fortawesome/fontawesome-svg-core",
         "@fortawesome/free-regular-svg-icons",
         "@fortawesome/react-fontawesome",
