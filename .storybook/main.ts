1b34ed334cee687c24d41530ecf20a9a164f60b9
Julian <julius.polar@protonmail.com>
2024-01-13 21:46:03 +0100

    updated yarn.lock, updated storybook addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../lib/**/*.mdx", "../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-actions",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-links",
-    "@storybook/addon-onboarding",
     "@storybook/addon-themes",
   ],
   framework: {
